8742b3b2d99d9982c6712495ef7aabff502bd065
Sryder13 <sryder13@gmail.com>
2017-10-26 20:04:05 +0100

    Star power translation tables Redid the part that handled the regular kart translation tables too, now uses the new table created instead of the horror of what was there before

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -5263,8 +5263,13 @@ static void HWR_ProjectSprite(mobj_t *thing)
 		// New colormap stuff for skins Tails 06-07-2002
 		if (thing->skin && thing->sprite == SPR_PLAY) // This thing is a player!
 		{
-			size_t skinnum = (skin_t*)thing->skin-skins;
-			vis->colormap = R_GetTranslationColormap((INT32)skinnum, thing->color, GTC_CACHE);
+			if (thing->player && thing->player->kartstuff[k_startimer])
+				vis->colormap = R_GetTranslationColormap(TC_STARMAN, thing->color, GTC_CACHE);
+			else
+			{
+				size_t skinnum = (skin_t*)thing->skin-skins;
+				vis->colormap = R_GetTranslationColormap((INT32)skinnum, thing->color, GTC_CACHE);
+			}
 		}
 		else
 			vis->colormap = R_GetTranslationColormap(TC_DEFAULT, vis->mobj->color ? vis->mobj->color : SKINCOLOR_CYAN, GTC_CACHE);
