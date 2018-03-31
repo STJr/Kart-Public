0f6c79d9834037eaf3fbbc92e7af4190dc96b209
TehRealSalt <tehrealsalt@gmail.com>
2018-03-31 00:12:26 -0400

    I'm a huge idiot

--- src/g_game.c
+++ src/g_game.c
@@ -3964,6 +3964,7 @@ void G_SaveGame(UINT32 savegameslot)
 //
 void G_DeferedInitNew(boolean pultmode, const char *mapname, INT32 pickedchar, UINT8 ssplayers, boolean FLS)
 {
+	INT32 i;
 	UINT8 color = 0;
 	paused = false;
 
@@ -3972,7 +3973,7 @@ void G_DeferedInitNew(boolean pultmode, const char *mapname, INT32 pickedchar, U
 	ghosts = NULL;
 
 	for (i = 0; i < NUMMAPS; i++)
-		randmapbuffer[i] = -1; // Reset this
+		randmapbuffer[i] = -1;
 
 	// this leave the actual game if needed
 	SV_StartSinglePlayerServer();
