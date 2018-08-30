f6ce2bfb54b7a4f5405b3b2be9617e3dbc2521da
TehRealSalt <tehrealsalt@gmail.com>
2018-08-30 17:53:25 -0400

    Ignore texture on horizon lines when in OGL
    Before: https://cdn.discordapp.com/attachments/300531341957529602/484842910475354124/kart0110.png
    After: https://cdn.discordapp.com/attachments/270211093761097728/484841628771680258/kart0109.png
    
    Not a solution, but better than the previous.

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -2041,7 +2041,8 @@ static void HWR_StoreWallRange(double startfrac, double endfrac)
 	{
 		// Single sided line... Deal only with the middletexture (if one exists)
 		gr_midtexture = R_GetTextureNum(gr_sidedef->midtexture);
-		if (gr_midtexture)
+		if (gr_midtexture
+			&& gr_linedef->special != 41) // Ignore horizon line for OGL
 		{
 			if (drawtextured)
 			{
