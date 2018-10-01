379772e5aa941c17e7105d953413b2d09365a7d8
toaster <rollerorbital@gmail.com>
2018-10-01 13:25:13 +0100

    Allow respawning spectators before starttime is up because they no longer spawn in a condition that allows them to get an unfair advantage (drop dash or its ghetto 2.0-esque predecessor)
    Also, some camera tweaks:
    * Force chasecam when exiting, just like it's forced when you're dead and not a spectator.
    * Force a horizontal camera angle aiming when spectator and dead, to avoid skybox bugs.

--- src/r_main.c
+++ src/r_main.c
@@ -1094,7 +1094,7 @@ void R_SetupFrame(player_t *player, boolean skybox)
 		chasecam = (cv_chasecam.value != 0);
 	}
 
-	if (player->climbing || (player->pflags & PF_NIGHTSMODE) || player->playerstate == PST_DEAD)
+	if (player->playerstate == PST_DEAD || player->exiting)
 		chasecam = true; // force chasecam on
 	else if (player->spectator) // no spectator chasecam
 		chasecam = false; // force chasecam off
