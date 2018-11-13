d09fd4b8611c5f06dd3c68c476312348406a2d60
TehRealSalt <tehrealsalt@gmail.com>
2018-11-13 01:09:36 -0500

    Carry over old explosion sfx flags

--- src/sounds.c
+++ src/sounds.c
@@ -424,7 +424,7 @@ sfxinfo_t S_sfx[NUMSFX] =
   {"s3k4b",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
   {"s3k4c",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
   {"s3k4d",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
-  {"s3k4e",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
+  {"s3k4e",  false,  64,  8, -1, NULL, 0,        -1,  -1, LUMPERROR}, // Kart explosion
   {"s3k4f",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
   {"s3k50",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
   {"s3k51",  false,  64,  0, -1, NULL, 0,        -1,  -1, LUMPERROR},
