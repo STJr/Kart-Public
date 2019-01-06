eebdda083338a7216883c6dd03473121b69b6a17
SeventhSentinel <jameshall.yahoosucks@gmail.com>
2019-01-06 17:17:52 -0500

    Minimum speed to get drift sparks

--- src/dehacked.c
+++ src/dehacked.c
@@ -8281,7 +8281,8 @@ static const char *const KARTSTUFF_LIST[] = {
 	"YOUGOTEM",
 
 	"ITEMBLINK",
-	"ITEMBLINKMODE"
+	"ITEMBLINKMODE",
+	"GETSPARKS"
 };
 
 static const char *const HUDITEMS_LIST[] = {
