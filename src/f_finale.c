b560a90890d476ced65737b22733f4cc2dfe66df
TehRealSalt <tehrealsalt@gmail.com>
2019-01-13 20:47:57 -0500

    Gag spacing was not updated

--- src/f_finale.c
+++ src/f_finale.c
@@ -556,7 +556,7 @@ static struct {
 	// This Tyler52 gag is troublesome
 	// Alignment should be ((spaces+1 * 100) + (headers+1 * 38) + (lines * 15))
 	// Current max image spacing: (200*17)
-	{112, (15*100)+(17*38)+(102*15), "TYLER52", SKINCOLOR_NONE},
+	{112, (15*100)+(17*38)+(72*15), "TYLER52", SKINCOLOR_NONE},
 	{0, 0, NULL, SKINCOLOR_NONE}
 };
 
