387a701db52260389758727e428dc6567053b855
James R <justsomejames2@gmail.com>
2019-04-18 23:50:29 -0700

    Add a "-noaudio" parm to cover "-nomusic" and "-nosound"

--- src/s_sound.c
+++ src/s_sound.c
@@ -2182,7 +2182,7 @@ static void Command_RestartAudio_f(void)
 
 void GameSounds_OnChange(void)
 {
-	if (M_CheckParm("-nosound"))
+	if (M_CheckParm("-nosound") || M_CheckParm("-noaudio"))
 		return;
 
 	if (sound_disabled)
@@ -2196,7 +2196,7 @@ void GameSounds_OnChange(void)
 
 void GameDigiMusic_OnChange(void)
 {
-	if (M_CheckParm("-nomusic"))
+	if (M_CheckParm("-nomusic") || M_CheckParm("-noaudio"))
 		return;
 	else if (M_CheckParm("-nodigmusic"))
 		return;
@@ -2239,7 +2239,7 @@ void GameDigiMusic_OnChange(void)
 #ifndef NO_MIDI
 void GameMIDIMusic_OnChange(void)
 {
-	if (M_CheckParm("-nomusic"))
+	if (M_CheckParm("-nomusic") || M_CheckParm("-noaudio"))
 		return;
 	else if (M_CheckParm("-nomidimusic"))
 		return;
