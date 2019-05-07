5022cde44323fd135d71483bbadae4ca155cd646
Sal <tehrealsalt@gmail.com>
2019-05-06 21:51:35 -0400

    Merge branch 'version-prep' into 'master'
    Version prep
    
    See merge request KartKrew/Kart!148
    
    (cherry picked from commit dfc4b22c911340e330a0f71fabb4a1461b5747ed)
    
    183e75ff Final v1 colors
    9629fb32 Modify the first shade of Byzantium
    39bea80b Add options for adjusting deadzone, increase default deadzone from 0.25 to 0.5
    f99a5a6a Fix wheel animations
    fd148ec2 Dumbass typo
    e772a750 Missed a spot
    3d71a0aa Merge branch 'next-colors' into 'v1'
    071b335d Merge branch 'anim-fix' into 'v1'
    0cd815cb Merge branch 'deadzone-bullshit' into 'v1'
    30c19caa Update version number
    8f07ed77 Update patch.kart hash

--- src/doomdef.h
+++ src/doomdef.h
@@ -149,12 +149,13 @@ extern FILE *logstream;
 // most interface strings are ignored in development mode.
 // we use comprevision and compbranch instead.
 #else
-#define VERSION    100 // Game version
-#define SUBVERSION 4 // more precise version number
-#define VERSIONSTRING "v1.0.4"
-#define VERSIONSTRINGW L"v1.0.4"
-// Hey! If you change this, add 1 to the MODVERSION below!
-// Otherwise we can't force updates!
+#define VERSION    110 // Game version
+#define SUBVERSION 0 // more precise version number
+#define VERSIONSTRING "v1.1"
+#define VERSIONSTRINGW L"v1.1"
+// Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
+// And change CMakeLists.txt, for CMake users!
+// AND appveyor.yml, for the build bots!
 #endif
 
 // Maintain compatibility with 1.0.x record attack replays?
@@ -224,7 +225,7 @@ extern FILE *logstream;
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
-#define MODVERSION 4
+#define MODVERSION 5
 
 // Filter consvars by version
 // To version config.cfg, MAJOREXECVERSION is set equal to MODVERSION automatically.
