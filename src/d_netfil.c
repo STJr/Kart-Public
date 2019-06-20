d5e1c1af425dec36676da9f7009c0d8e41048268
James R <justsomejames2@gmail.com>
2019-06-20 15:24:50 -0700

    Add a comment by Sryder's request

--- src/d_netfil.c
+++ src/d_netfil.c
@@ -130,6 +130,7 @@ UINT8 *PutFileNeeded(UINT16 firstfile)
 
 		nameonly(strcpy(wadfilename, wadfiles[i]->filename));
 
+		// Look below at the WRITE macros to understand what these numbers mean.
 		if (p + 1 + 4 + strlen(wadfilename) + 1 + 16 > p_start + MAXFILENEEDED)
 		{
 			// Too many files to send all at once
