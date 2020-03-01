1959a9b1072f123dbd029ef1baf81270472c4fdd
James R <justsomejames2@gmail.com>
2020-03-01 11:19:59 -0800

    Don't show "All" room when hosting

--- src/mserv.c
+++ src/mserv.c
@@ -99,7 +99,7 @@
 
 /* HTTP */
 int  HMS_in_use (void);
-void HMS_fetch_rooms (void);
+void HMS_fetch_rooms (int joining);
 int  HMS_register (void);
 void HMS_unlist (void);
 void HMS_update (void);
@@ -498,7 +498,7 @@ INT32 GetRoomsList(boolean hosting)
 
 	if (HMS_in_use())
 	{
-		HMS_fetch_rooms();
+		HMS_fetch_rooms( ! hosting );
 		return 1;
 	}
 
