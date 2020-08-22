4210352be92b92bb83bb517005f8ecef815fd332
Hannu Hanhi <hhanhipublic@gmail.com>
2020-08-23 00:10:50 +0300

    Fix timedemo in OpenGL mode

--- src/hardware/hw_main.c
+++ src/hardware/hw_main.c
@@ -4733,6 +4733,7 @@ void HWR_RenderPlayerView(INT32 viewnumber, player_t *player)
 	}
 
 	R_SetupFrame(player, false); // This can stay false because it is only used to set viewsky in r_main.c, which isn't used here
+	framecount++; // for timedemo
 	HWR_RenderFrame(viewnumber, player, false);
 }
 
