7ab1c4a2df835cd08fc2cfb1be7743e6a7bad6d7
James R <justsomejames2@gmail.com>
2020-08-30 19:06:31 -0700

    Update versions to 1.3

--- src/doomdef.h
+++ src/doomdef.h
@@ -148,9 +148,9 @@ extern char  logfilename[1024];
 // we use comprevision and compbranch instead.
 #else
 #define VERSION    1 // Game version
-#define SUBVERSION 2 // more precise version number
-#define VERSIONSTRING "v1.2"
-#define VERSIONSTRINGW L"v1.2"
+#define SUBVERSION 3 // more precise version number
+#define VERSIONSTRING "v1.3"
+#define VERSIONSTRINGW L"v1.3"
 // Hey! If you change this, add 1 to the MODVERSION below! Otherwise we can't force updates!
 // And change CMakeLists.txt, for CMake users!
 // AND appveyor.yml, for the build bots!
@@ -223,7 +223,7 @@ extern char  logfilename[1024];
 // it's only for detection of the version the player is using so the MS can alert them of an update.
 // Only set it higher, not lower, obviously.
 // Note that we use this to help keep internal testing in check; this is why v2.1.0 is not version "1".
-#define MODVERSION 6
+#define MODVERSION 7
 
 // Filter consvars by version
 // To version config.cfg, MAJOREXECVERSION is set equal to MODVERSION automatically.
