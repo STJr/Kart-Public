df769a13e4d584618e3d38d6183ce9e50028d3c5
JugadorXEI <eugeniom9@gmail.com>
2021-05-20 19:39:32 +0200

    Fix grown players no spark/wrongdrift interaction with yellow spring panels

--- src/k_kart.c
+++ src/k_kart.c
@@ -5061,6 +5061,13 @@ INT32 K_GetKartDriftSparkValue(player_t *player)
 static void K_KartDrift(player_t *player, boolean onground)
 {
 	fixed_t minspeed = (10 * player->mo->scale);
+
+	// Grown players taking yellow spring panels will go below minspeed for one tic,
+	// and will then wrongdrift or have their sparks removed because of this.
+	// This fixes this problem.
+	if (player->kartstuff[k_pogospring] == 2 && player->mo->scale > FRACUNIT)
+		minspeed = (10 * FRACUNIT);
+
 	INT32 dsone = K_GetKartDriftSparkValue(player);
 	INT32 dstwo = dsone*2;
 	INT32 dsthree = dstwo*2;
