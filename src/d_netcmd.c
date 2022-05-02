c1c88c0de7a75f14176f64c43237e0ac89580b9b
Sally Coolatta <tehrealsalt@gmail.com>
2022-05-01 20:00:41 -0400

    Remove unused variable
    Makes me unable to compile with harsh compiler settings

--- src/d_netcmd.c
+++ src/d_netcmd.c
@@ -2446,7 +2446,6 @@ void D_PickVote(void)
 	SINT8 templevels[MAXPLAYERS];
 	SINT8 votecompare = -1;
 	UINT8 numvotes = 0, key = 0;
-	boolean force = true;
 	INT32 i;
 
 	for (i = 0; i < MAXPLAYERS; i++)
@@ -2460,8 +2459,6 @@ void D_PickVote(void)
 			numvotes++;
 			if (votecompare == -1)
 				votecompare = votes[i];
-			else if (votes[i] != votecompare)
-				force = false;
 		}
 	}
 
