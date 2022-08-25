d86081cb8aee1f2fa310869504feed7b25fe0933
toaster <rollerorbital@gmail.com>
2022-08-25 18:00:19 +0100

    If the client has caught up to the server -- say, during a wipe -- make GetLag return a ping of zero
    Should fix the momentarily large ping bug.

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -6221,6 +6221,9 @@ INT32 D_NumPlayers(void)
 
 tic_t GetLag(INT32 node)
 {
+	// If the client has caught up to the server -- say, during a wipe -- lag is meaningless.
+	if (nettics[node] > gametic)
+		return 0;
 	return gametic - nettics[node];
 }
 
