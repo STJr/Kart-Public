cb575bcde99ac49c3b22f37df408e1f6f83d1bcc
toaster <rollerorbital@gmail.com>
2022-10-28 18:02:50 +0100

    Add additional external contributors.

--- src/f_finale.c
+++ src/f_finale.c
@@ -431,10 +431,12 @@ static const char *credits[] = {
 	"\"JugadorXEI\"",
 	"\"Kimberly\"",
 	"\"Lighto97\"",
+	"\"Lonsfor\"",
 	"\"mazmazz\"",
 	"\"minenice\"",
 	"\"Shuffle\"",
 	"\"Snu\"",
+	"\"X.organic\"",
 	"",
 	"\1Lead Artists",
 	"Desmond \"Blade\" DesJardins",
@@ -560,7 +562,7 @@ static struct {
 	// This Tyler52 gag is troublesome
 	// Alignment should be ((spaces+1 * 100) + (headers+1 * 38) + (lines * 15))
 	// Current max image spacing: (216*17)
-	{112, (16*100)+(19*38)+(101*15), "TYLER52", SKINCOLOR_NONE},
+	{112, (16*100)+(19*38)+(103*15), "TYLER52", SKINCOLOR_NONE},
 	{0, 0, NULL, SKINCOLOR_NONE}
 };
 
