e57fa80e41e404635a3c0d2da79d1740abd66d0b
Sky <sky@leite.dev>
2023-07-04 20:04:24 -0300

    Add missing ;

--- src/m_menu.c
+++ src/m_menu.c
@@ -10178,7 +10178,7 @@ static void M_DrawJoystick(void)
 		compareval4 = cv_usejoystick4.value;
 		compareval3 = cv_usejoystick3.value;
 		compareval2 = cv_usejoystick2.value;
-		compareval = cv_usejoystick.value
+		compareval = cv_usejoystick.value;
 #endif
 
 		if ((setupcontrolplayer == 4 && (i == compareval4))
