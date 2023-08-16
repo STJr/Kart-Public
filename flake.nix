ba8b5857e825ad1982cd8ce50affd027cf009236
Sky <sky@leite.dev>
2023-08-16 15:05:18 -0300

    Build srb2kart by default

--- flake.nix
+++ flake.nix
@@ -6,11 +6,12 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       srb2kart-wasm = pkgs.callPackage ./emscripten.nix { };
       srb2kart-web = pkgs.callPackage ./src/web/default.nix { };
+      srb2kart =
+        pkgs.callPackage ./default.nix { inherit srb2kart-wasm srb2kart-web; };
     in {
       packages.x86_64-linux.wasm = srb2kart-wasm;
       packages.x86_64-linux.web = srb2kart-web;
-      packages.x86_64-linux.srb2kart =
-        pkgs.callPackage ./default.nix { inherit srb2kart-wasm srb2kart-web; };
+      packages.x86_64-linux.default = srb2kart;
 
       # devShells.x86_64-linux.default = pkgs.callPackage ./shell.nix { };
     };
