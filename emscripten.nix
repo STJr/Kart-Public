f3a566feca3c0fbb663285efb8cc2fbaec34b2fa
Sky <sky@leite.dev>
2023-08-16 16:27:37 -0300

    Vendor emscripten properly

--- emscripten.nix
+++ emscripten.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl, fetchFromGitHub, substituteAll, cmake, curl, nasm
 , unzip, game-music-emu, libpng, SDL2, SDL2_mixer, zlib, buildEmscriptenPackage
 , libelf, openssl, emscripten, emscriptenPackages, libogg, emscriptenStdenv
-, breakpointHook, debug ? true }:
+, breakpointHook, unar, debug ? true }:
 
 let
 
@@ -16,29 +16,27 @@ let
     sha256 = "sha256-ejhPuZ1C8M9B0S4+2HN1T5pbormT1eVL3nlivqOszdE=";
   };
 
-  sdl2Port = fetchurl {
-    url = "https://github.com/libsdl-org/SDL/archive/release-2.24.2.zip";
-    sha256 = "sha256-3DPf2yTCi3V/MjReqOymemAwydywhK6MeLwnoh5zeto=";
-  };
-
-  zlibPort = fetchurl {
-    url = "https://github.com/madler/zlib/archive/refs/tags/v1.2.13.tar.gz";
-    sha256 = "sha256-FSWVKgpWdYF5JhOpcjMz1/jMILh6gfkg+4vH4/IlFCg=";
-  };
-
-  oggPort = fetchurl {
-    url = "https://github.com/emscripten-ports/ogg/archive/version_1.zip";
-    sha256 = "sha256-IvWZu+YGX/mPxWXy22Zl2lSBgc+QmCuJ/cQtg/vGNws=";
-  };
-
-  vorbisPort = fetchurl {
-    url = "https://github.com/emscripten-ports/vorbis/archive/version_1.zip";
-    sha256 = "sha256-snyqUYYgCLEJyxN18DnXb2ItYOpzrTCOG1oJh0Vp/n4=";
-  };
-
-  sdl2MixerPort = fetchurl {
-    url = "https://github.com/libsdl-org/SDL_mixer/archive/release-2.0.4.zip";
-    sha256 = "sha256-4Ajep3iVJ89tEz+DiZfTL/t2E3W/pCUCspkUUa7TxFg=";
+  ports = {
+    ogg = {
+      url = "https://github.com/emscripten-ports/ogg/archive/version_1.zip";
+      sha256 = "sha256-IvWZu+YGX/mPxWXy22Zl2lSBgc+QmCuJ/cQtg/vGNws=";
+    };
+    sdl2 = {
+      url = "https://github.com/libsdl-org/SDL/archive/release-2.24.2.zip";
+      sha256 = "sha256-3DPf2yTCi3V/MjReqOymemAwydywhK6MeLwnoh5zeto=";
+    };
+    zlib = {
+      url = "https://github.com/madler/zlib/archive/refs/tags/v1.2.13.tar.gz";
+      sha256 = "sha256-FSWVKgpWdYF5JhOpcjMz1/jMILh6gfkg+4vH4/IlFCg=";
+    };
+    vorbis = {
+      url = "https://github.com/emscripten-ports/vorbis/archive/version_1.zip";
+      sha256 = "sha256-snyqUYYgCLEJyxN18DnXb2ItYOpzrTCOG1oJh0Vp/n4=";
+    };
+    sdl2_mixer = {
+      url = "https://github.com/libsdl-org/SDL_mixer/archive/release-2.0.4.zip";
+      sha256 = "sha256-4Ajep3iVJ89tEz+DiZfTL/t2E3W/pCUCspkUUa7TxFg=";
+    };
   };
 
 in stdenv.mkDerivation rec {
@@ -47,39 +45,49 @@ in stdenv.mkDerivation rec {
 
   src = ./.;
 
-  nativeBuildInputs = [ cmake nasm unzip patchedEmscripten ];
+  nativeBuildInputs = [ cmake nasm unzip unar emscripten ];
 
   buildInputs = [ curl emscriptenPackages.zlib libogg ];
 
   cmakeFlags = [
     "-DCMAKE_INSTALL_PREFIX=$out"
     "-DCMAKE_BUILD_TYPE=${if debug then "Debug" else "Release"}"
-    "-DCMAKE_C_COMPILER=${patchedEmscripten}/bin/emcc"
+    "-DCMAKE_C_COMPILER=${emscripten}/bin/emcc"
   ];
 
   dontStrip = debug;
 
-  NIX_LOCAL_PORTS =
-    "zlib=${zlibPort},sdl2=${sdl2Port},ogg=${oggPort},vorbis=${vorbisPort},sdl2_mixer=${sdl2MixerPort}";
+  # NIX_LOCAL_PORTS =
+  #   "zlib=${zlibPort},sdl2=${sdl2Port},ogg=${oggPort},vorbis=${vorbisPort},sdl2_mixer=${sdl2MixerPort}";
 
   configurePhase = ''
     runHook preConfigure
 
-    SRCDIR=$(pwd)
-
     mkdir -p $out
-
-    export EM_CACHE=$(pwd)/.emscriptencache
-    mkdir -p $EM_CACHE
 
     mkdir build
     pushd build
-    emcmake cmake ${toString cmakeFlags} "$SRCDIR"
+    echo $EM_CACHE
+    emcmake cmake ${toString cmakeFlags} ..
 
     runHook postConfigure
   '';
 
   preConfigure = ''
+    export EM_CACHE=$TMPDIR/.emscriptencache
+
+    mkdir -p $EM_CACHE/ports
+
+    pushd $EM_CACHE/ports
+    ${lib.concatStrings (builtins.attrValues (builtins.mapAttrs (name: value: ''
+      mkdir ${name}
+      pushd ${name}
+      echo ${value.url} > .emscripten_url
+      unar ${builtins.fetchurl value}
+      popd
+    '') ports))}
+    popd
+
     mkdir -p assets/installer
     pushd assets/installer
     unzip ${assets} "*.kart" srb2.srb
